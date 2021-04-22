334abc5f68734e02a4bedec716e7a389e26ca61b
paulcarlton-ww <paul.carlton@weave.works>
2021-04-22 10:28:35 +0100

    update clone.sh to support forks

--- bin/clone.sh
+++ bin/clone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Utility for comparing files in different directories
+# Utility for cloning github repos
 # Version: 1.0
 # Author: Paul Carlton (mailto:paul.carlton@weave.works)
 
@@ -8,7 +8,7 @@ set -euo pipefail
 
 function usage()
 {
-    echo "usage ${0} [--dry-run] [--debug] <path>"
+    echo "usage ${0} [--dry-run] [--debug] [--fork] <path>"
     echo "where <path> is the repository path, i.e. github.com/weaveworks/wksctl"
     echo "This script will create the appropriate directory sub tree under $HOME/go/src"
     echo "then clones the repository if the final element of the path does not exist"
@@ -18,9 +18,8 @@ function usage()
 
 function args() {
   dry_run=""
-  #unset force
+  unset fork
   unset path
-  #unset debug
 
   arg_list=( "$@" )
   arg_count=${#arg_list[@]}
@@ -29,7 +28,7 @@ function args() {
     case "${arg_list[${arg_index}]}" in
           "--debug") set -x;;
         "--dry-run") dry_run="echo ";;
-        #  "--force") force=1;;
+          "--fork") fork=1;;
                "-h") usage; exit;;
            "--help") usage; exit;;
                "-?") usage; exit;;
@@ -52,13 +51,20 @@ args "$@"
 
 repo=$(basename "${HOME}/go/src/${path}")
 dir=$(dirname "${HOME}/go/src/${path}")
+org=$(basename "${dir}")
+if [ -n "${fork}" ] ; then
+  org=paulcarlton-ww
+fi
 
 if [ ! -d "${HOME}/go/src/${path}" ] ; then
     $dry_run mkdir -p "${HOME}/go/src/${path}"
-    $dry_run git clone git@github.com:"$(basename "${dir}")"/"${repo}".git "${HOME}/go/src/${path}"
+    $dry_run git clone git@github.com:"${org}"/"${repo}".git "${HOME}/go/src/${path}"
 fi
 
+echo "${HOME}/go/src/${path}"
+
 $dry_run git -C "${HOME}/go/src/${path}" pull
+
 
 
 
